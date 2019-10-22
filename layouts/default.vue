174596f465b63b29eec5a941f3bf79a8c280307b
pochon-tech <pochon.tech.0519@gmail.com>
2019-10-22 13:56:29 +0900

    fix index

--- layouts/default.vue
+++ layouts/default.vue
@@ -100,11 +100,6 @@ export default {
           icon: 'apps',
           title: 'Welcome',
           to: '/'
-        },
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
         }
       ],
       miniVariant: false,
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,27 @@
 <template>
   <v-content>
     <v-app-bar>
-      <v-toolbar-title headline>Todo</v-toolbar-title>
-      <v-spacer />
+      <v-toolbar-title headline>Todo List</v-toolbar-title>
     </v-app-bar>
     <v-divider />
     <v-card flat tile>
-      <v-card-text>
-        Todo
-      </v-card-text>
+      <v-card-text><v-text-field v-model="content" placeholder="Todoを入力"/></v-card-text>
     </v-card>
+    <v-list>
+
+    </v-list>
     <v-divider />
   </v-content>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      todos: [],
+      content: ''
+    }
+  },
   components: {
   }
 }
